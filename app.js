f5159f37dc757a3be4317b5dc54428a0970351da
Simon Holmes <si@simonholmes.com>
2017-02-12 12:43:22 +0000

    Reverting to blank Express project

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-const index = require('./app_server/routes/index');
-const users = require('./app_server/routes/users');
+var index = require('./routes/index');
+var users = require('./routes/users');
 
-const app = express();
+var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
@@ -27,7 +27,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  const err = new Error('Not Found');
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
--- app_server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctrlMain = require('../controllers/main');
-
-/* GET home page. */
-router.get('/', ctrlMain.index);
-
-module.exports = router;
