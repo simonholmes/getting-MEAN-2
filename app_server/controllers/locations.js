537065c8ba247d1722bda8e7f4477c4954f3bfb4
Simon Holmes <si@simonholmes.com>
2017-02-19 18:42:07 +0000

    Create location info page and styles

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -5,7 +5,7 @@ const homelist = function(req, res){
 
 /* GET 'Location info' page */
 const locationInfo = function(req, res){
-  res.render('index', { title: 'Location info' });
+  res.render('location-info', { title: 'Location info' });
 };
 
 /* GET 'Add review' page */
