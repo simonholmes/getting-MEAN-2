e98bf0b60af8f9da0f5486e6da5f978973eb4417
Simon Holmes <si@simonholmes.com>
2017-02-19 20:11:50 +0000

    Move homepage data into controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,32 @@
 /* GET 'home' page */
 const homelist = function(req, res){
-  res.render('locations-list', { title: 'Home' });
+  res.render('locations-list', {
+    title: 'Loc8r - find a place to work with wifi',
+    pageHeader: {
+      title: 'Loc8r',
+      strapline: 'Find places to work with wifi near you!'
+    },
+    sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+    locations: [{
+        name: 'Starcups',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '100m'
+    }, {
+        name: 'Cafe Hero',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 4,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '200m'
+    }, {
+        name: 'Burger Queen',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 2,
+        facilities: ['Food', 'Premium wifi'],
+        distance: '250m'
+    }]
+  });
 };
 
 /* GET 'Location info' page */
