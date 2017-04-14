beda6a4110a28dbc5724a06fa009ee4839201785
Simon Holmes <si@simonholmes.com>
2017-04-14 08:00:23 +0100

    Create chapter 5 code

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-const dbURI = 'mongodb://localhost/Loc8r';
+let dbURI = 'mongodb://localhost/Loc8r';
+if (process.env.NODE_ENV === 'production') {
+  dbURI = process.env.MONGODB_URI;
+}
 mongoose.connect(dbURI);
 
 mongoose.connection.on('connected', () => {
@@ -37,3 +40,5 @@ process.on('SIGTERM', () => {
     process.exit(0);
   });
 });
+
+require('./locations');
---
+++ app_server/models/locations.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+const openingTimeSchema = new mongoose.Schema({
+  days: {
+    type: String,
+    required: true
+  },
+  opening: String,
+  closing: String,
+  closed: {
+    type: Boolean,
+    required: true
+  }
+});
+
+const reviewSchema = new mongoose.Schema({
+  author: String,
+  rating: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 5
+  },
+  reviewText: String,
+  createdOn: {
+    type: Date,
+    'default': Date.now
+  }
+});
+
+const locationSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  address: String,
+  rating: {
+    type: Number,
+    'default': 0,
+    min: 0,
+    max: 5
+  },
+  facilities: [String],
+  coords: {
+    type: [Number],
+    index: '2dsphere'
+  },
+  openingTimes: [openingTimeSchema],
+  reviews: [reviewSchema]
+});
