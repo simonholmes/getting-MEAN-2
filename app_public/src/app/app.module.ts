b83db3989c4d1804b11e6dd8fd2180aa39b90509
Simon Holmes <si@simonholmes.com>
2017-08-02 11:24:22 +0100

    Add reusable sidebar component

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { FrameworkComponent } from './framework/framework.component';
 import { AboutComponent } from './about/about.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { PageHeaderComponent } from './page-header/page-header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { PageHeaderComponent } from './page-header/page-header.component';
     FrameworkComponent,
     AboutComponent,
     HomepageComponent,
-    PageHeaderComponent
+    PageHeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
--- app_public/src/app/homepage/homepage.component.ts
+++ app_public/src/app/homepage/homepage.component.ts
@@ -16,7 +16,8 @@ export class HomepageComponent implements OnInit {
     header : {
       title : 'Loc8r',
       strapline : 'Find places to work with wifi near you!'
-    }
+    },
+    sidebar : 'Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you\'re looking for.'
   };
 
 }
