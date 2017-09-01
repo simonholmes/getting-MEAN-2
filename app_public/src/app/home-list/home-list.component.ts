af1ecd75aea5c09fbfc7e78f803d6cb17a4cc9ec
Simon Holmes <si@simonholmes.com>
2017-09-01 07:54:48 +0100

    A bit of tidying

--- app_public/src/app/home-list/home-list.component.ts
+++ app_public/src/app/home-list/home-list.component.ts
@@ -22,21 +22,6 @@ export class HomeListComponent implements OnInit {
   constructor(private loc8rDataService: Loc8rDataService, private geolocationService: GeolocationService) { }
 
   locations: Location[];
-  // locations: Location[] = [{
-  //   _id: '590d8dc7a7cb5b8e3f1bfc48',
-  //   name: 'Costy',
-  //   distance: 14000.1234,
-  //   address: 'High Street, Reading',
-  //   rating: 3,
-  //   facilities: ['hot drinks', 'food', 'power']
-  // }, {
-  //   _id: '590d8dc7a7cb5b8e3f1bfc48',
-  //   name: 'Starcups',
-  //   distance: 120.542,
-  //   address: 'High Street, Reading',
-  //   rating: 5,
-  //   facilities: ['wifi', 'food', 'hot drinks']
-  // }];
 
   message: string;
 
