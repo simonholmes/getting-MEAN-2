fbe40661bc1ddd421d5efd463063b5d4b4b7d9c6
Bryan Heisler <bheisler@alumni.uoguelph.ca>
2018-11-14 20:34:37 -0500

    10.4

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 require('./app_api/models/db');
 
-const index = require('./app_server/routes/index');
+//const index = require('./app_server/routes/index');
 const apiRoutes = require('./app_api/routes/index');
 
 const app = express();
@@ -21,8 +21,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'app_public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_public', 'build')));
 
 app.use('/api', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
@@ -30,8 +30,11 @@ app.use('/api', function(req, res, next) {
   next();
 });
 
-app.use('/', index);
+//app.use('/', index);
 app.use('/api', apiRoutes);
+app.get('*', function(req, res, next) {
+  res.sendFile(path.join(__dirname, 'app_public', 'build', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
